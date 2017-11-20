98b89c0f655c119d50e565796d28225c36d1995c
IbnuAhsani <ibnu.muhari@gmail.com>
2017-11-21 00:19:51 +0700

    Update phpmyadmin Database v.2

--- Mockup, Database & Other Stuff/hooneyan.sql
+++ Mockup, Database & Other Stuff/hooneyan.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 17 Nov 2017 pada 09.52
--- Versi Server: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Nov 20, 2017 at 06:18 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,13 +25,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `agen`
+-- Table structure for table `agen`
 --
 
 CREATE TABLE `agen` (
   `id_agen` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL,
   `nama_agen` varchar(50) NOT NULL,
+  `tipe_akun` int(11) NOT NULL,
   `tgl_lahir_agen` date NOT NULL,
   `email_agen` varchar(100) NOT NULL,
   `no_kontak_agen` varchar(20) NOT NULL
@@ -38,7 +41,7 @@ CREATE TABLE `agen` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `properti`
+-- Table structure for table `properti`
 --
 
 CREATE TABLE `properti` (
@@ -60,7 +63,7 @@ CREATE TABLE `properti` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi`
+-- Table structure for table `transaksi`
 --
 
 CREATE TABLE `transaksi` (
@@ -73,30 +76,39 @@ CREATE TABLE `transaksi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id_user` int(5) NOT NULL,
   `password` varchar(50) NOT NULL,
   `nama_user` varchar(100) NOT NULL,
+  `tipe_akun` int(1) NOT NULL,
   `tgl_lahir` date NOT NULL,
   `email` varchar(100) NOT NULL,
   `no_kontak` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `password`, `nama_user`, `tgl_lahir`, `email`, `no_kontak`) VALUES
-(1, '827ccb0eea8a706c4c34a16891f84e7b', 'Muhammad Affandi', '0000-00-00', 'affandim354@gmail.com', ''),
-(2, '827ccb0eea8a706c4c34a16891f84e7b', 'Asia', '0000-00-00', 'afnd@gmail.com', ''),
-(3, '827ccb0eea8a706c4c34a16891f84e7b', 'Asuie', '0000-00-00', 'aa@g.to', '');
+INSERT INTO `user` (`id_user`, `password`, `nama_user`, `tipe_akun`, `tgl_lahir`, `email`, `no_kontak`) VALUES
+(1, '827ccb0eea8a706c4c34a16891f84e7b', 'Muhammad Affandi', 0, '0000-00-00', 'affandim354@gmail.com', ''),
+(2, '827ccb0eea8a706c4c34a16891f84e7b', 'Asia', 0, '0000-00-00', 'afnd@gmail.com', ''),
+(3, '827ccb0eea8a706c4c34a16891f84e7b', 'Asuie', 0, '0000-00-00', 'aa@g.to', ''),
+(4, 'c1257a2e0fb0b3d8a83eaa0a3d1703c8', 'Ibnu Ahsani', 0, '0000-00-00', 'ibnu.muhari@gmail.com', ''),
+(6, '827ccb0eea8a706c4c34a16891f84e7b', 'Hello World', 1, '2017-05-17', 'hello.world@gmail.com', '12345');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `agen`
+--
+ALTER TABLE `agen`
+  ADD PRIMARY KEY (`id_agen`);
 
 --
 -- Indexes for table `properti`
@@ -124,7 +136,9 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
