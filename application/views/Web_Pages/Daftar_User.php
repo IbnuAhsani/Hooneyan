ab572015dbbdbd023c9209cbbf638596c5e51b58
IbnuAhsani <ibnu.muhari@gmail.com>
2017-12-10 00:08:03 +0700

    Update UI Login & Daftar User

--- application/views/Web_Pages/Daftar_User.php
+++ application/views/Web_Pages/Daftar_User.php
@@ -1,38 +1,98 @@
 <html>
 <head>
+	<link href="https://fonts.googleapis.com/css?family=Courgette|Lobster" rel="stylesheet">	
 	<title>Daftar</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/Final.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/Final3.css">	
 </head>
 <body class="login_page">
 	<div class="container">
 		<?php
-         $error_msg=$this->session->flashdata('error_msg');
-         if($error_msg)
-	         {
-	           echo $error_msg;
-	         }
-      ?>
+	     $error_msg=$this->session->flashdata('error_msg');
+	     if($error_msg)
+	       {
+	         echo $error_msg;
+	       }
+    ?>
 		<form id="user_data" role="form" class="form-signin" method="POST" action="<?php echo base_url('index.php/Daftar_User/register_user'); ?>">
-			<h1 class="form-signin-heading text-muted">Daftar Akun Hooneyan</h1><br>
-			<input type="text" placeholder="Nama Lengkap" name="nama" required>
-			<input type="email" placeholder="Email Anda" name="email" required>
-			<input type="password" placeholder="Password" name="password" required>
-			<input type="text" placeholder="Nomor Telpon 1" name="no_kontak_1" required>
-			<input type="text" placeholder="Nomor Telpon 2" name="no_kontak_2">
-			<input type="text" placeholder="Nomor Telpon 3" name="no_kontak_3">
-			<textarea rows="4" cols="50" name="alamat" form="user_data" placeholder="Alamat"></textarea>
-			<br>
-			<label for="tipe_akun" style="color: white">Tipe Akun</label><br>
-		  <input type="radio" name="tipe_akun" value="2"><span style="color: white">User</span><br>
-		  <input type="radio" name="tipe_akun" value="1"><span style="color: white">Agen</span><br><br>
-		  <label for="tgl_lhr" style="color: white">Tanggal Lahir : </label>
-		  <input type="date" name="tgl_lahir"><br><br>		
-			<center><input type="submit" value="Daftar" name="register"></center>
+			<h1 class="form-signin-heading font-only" style="margin: 0">Daftar Akun</h1>
+			<h1 class="form-signin-heading font-logo" style="margin: 0">Hooneyan</h1><br>
+				<table>
+					<tbody>
+						<tr>
+							<td valign="top">
+								<label class="font-only" style="color: white">Nama Lengkap</label><br>
+								<input type="text" name="nama" required>
+							</td>
+							<td class="spaced-col"></td>
+							<td>
+									<label class="font-only" style="color: white">Tanggal Lahir</label><br>								
+								  <input type="date" name="tgl_lahir"><br><br>		
+							</td>
+						</tr>
+						<tr class="spaced-row"></tr>
+						<tr>
+							<td valign="top">
+								<label class="font-only" style="color: white">Tipe Akun</label><br>
+							  <input type="radio" name="tipe_akun" value="2"><span style="color: white">User</span><br>
+							  <input type="radio" name="tipe_akun" value="1"><span style="color: white">Agen</span><br><br>
+							</td>
+							<td class="spaced-col"></td>
+							<td>
+									<label class="font-only" style="color: white">Alamat</label><br>								
+									<textarea rows="4" cols="21" name="alamat" form="user_data" placeholder="Alamat"></textarea>
+							</td>
+						</tr>
+						<tr class="spaced-row"></tr>
+						<tr>
+							<td valign="top">
+								<label class="font-only" style="color: white">Email</label><br>
+								<input type="email" name="email" required>
+							</td>
+							<td class="spaced-col"></td>
+							<td>
+									<label class="font-only" style="color: white">Password</label><br>								
+									<input type="password" name="password" required>
+							</td>
+						</tr>
+						<tr class="spaced-row"></tr>
+						<tr>
+							<td valign="top">
+								<label class="font-only" style="color: white">No Kontak 1</label><br>
+								<input type="text" name="no_kontak_1" required>
+							</td>
+							<td class="spaced-col"></td>
+							<td>
+									<label class="font-only" style="color: white">No Kontak 2</label><br>								
+									<input type="text" name="no_kontak_2">
+							</td>
+						</tr>
+						<tr class="spaced-row"></tr>
+						<tr>
+							<td valign="top">
+								<label class="font-only" style="color: white">No Kontak 3</label><br>
+								<input type="text" name="no_kontak_3">
+							</td>
+							<td class="spaced-col"></td>
+							<td></td>
+						</tr>
+						<tr class="spaced-row"></tr>	
+						<tr>
+							<td valign="top">
+							<center><input type="submit" value="Daftar" name="register"></center>
+							</td>
+							<td class="spaced-col"></td>
+							<td></td>
+						</tr>
+						<tr class="spaced-row"></tr>																														
+					</tbody>
+				</table>
 		</form>
-			<!--for centered text-->
 		<center>
-			<b style="color:white">Sudah punya Akun?</b><br>
-			</b><a href="<?php echo base_url('index.php/Login'); ?>" style="color: white"><u>Masuk disini</u></a>
+			<font class="font-only" style="color: white; font-size: 20px">Sudah punya Akun?</font><br>
+			<a class="font-only" href="<?php echo base_url('index.php/Login'); ?>" style="color: white; font-size: 20px">
+				<u>Masuk disini</u>
+			</a>
 		</center>
 	</div>
 </body>
--- application/views/Web_Pages/Login.php
+++ application/views/Web_Pages/Login.php
@@ -7,18 +7,27 @@
  ?>
 <html>
 <head>
+	<link href="https://fonts.googleapis.com/css?family=Courgette|Lobster" rel="stylesheet">
 	<title>Login</title>
-   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/Final.css">
-
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/Final.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/Final3.css">
 </head>
 <body class="login_page">
 	<div class="container">
 		<form class="form-signin" role="form" method="post" action="<?php echo base_url('index.php/Login/login_user'); ?>">
-			<h1 class="form-signin-heading text-muted">Selamat Datang di Hooneyan</h1>
+			<h1 class="form-signin-heading font-only" style="margin: 0">Selamat Datang di</h1>
+			<h1 class="form-signin-heading font-logo" style="margin-top: 0">Hooneyan</h1>
 			<input type="text" class="form-control" placeholder="Masukkan Email Anda" name="email" required="" autofocus>
 			<input type="password" class="form-control" placeholder="Masukkan Password Anda" name="password" required>
 			<button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
-			<p style="color: white">Belum punya akun? Klik <a href="<?php echo base_url('/index.php/Daftar_user') ?>" style="color: white;"><u>disini</u><a></p>
+			<br><br>
+			<font class="font-only" style="color: white; font-size: 20px; text-align: center;">
+				Belum punya akun?<br>
+				Klik 
+				<a href="<?php echo base_url('/index.php/Daftar_user') ?>" style="color: white;">
+					<u>disini</u>
+				<a>
+			</font>
 		</form>
 	</div>
 </body>
